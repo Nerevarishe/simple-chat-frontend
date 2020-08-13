e19c02b8a32ace8325f6a25543b345e35904fa69
Nerevarishe <JIyHaTuK@gmail.com>
2020-08-13 14:00:52 +0300

    adding withContext HOC

---
+++ src/hoc/withContext.js
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { Context } from "../globalStore/store";
+
+const withContext = (WrappedObject) => {
+  return (props) => {
+    // eslint-disable-next-line no-unused-vars
+    const [state, dispatch] = useContext(Context);
+    return (
+      <WrappedObject {...props} state={state.components}>
+        {props.children}
+      </WrappedObject>
+    );
+  };
+};
+
+export default withContext;
