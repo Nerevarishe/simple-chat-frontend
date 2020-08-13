21329625876cdd3e9b43b0c2bd30154aceb7c16d
Nerevarishe <JIyHaTuK@gmail.com>
2020-08-13 12:20:06 +0300

    dev

--- src/containers/Layout/Footer/FooterButtonWrapper.js
+++ src/containers/Layout/Footer/FooterButtonWrapper.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { rem } from "../../../utils/utils";
 
-const FooterButtonWrapper = styled.div`
+const FooterButtonWrapper = styled.li`
   width: ${rem("40px")};
   height: ${rem("40px")};
   font-size: ${rem("40px")};
--- src/containers/Layout/Footer/FooterStyled.js
+++ src/containers/Layout/Footer/FooterStyled.js
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 
-const FooterStyled = styled.div`
+const FooterStyled = styled.ul`
   display: flex;
   justify-content: space-around;
   align-items: center;
+  margin: 0;
+  padding: 0;
   width: 100%;
   height: 100%;
   background: ${(props) => props.theme.colors.blackColor};
+  list-style: none;
 `;
 
 export default FooterStyled;
