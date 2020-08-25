3ec2cb639a84e5ae6edb8dd7bf027a3efae24844
Nerevarishe <JIyHaTuK@gmail.com>
2020-08-25 14:04:45 +0300

    overflow-wrap: break-word

--- src/components/cards/ChatMessageCard/ChatMessage/ChatMessageStyled.js
+++ src/components/cards/ChatMessageCard/ChatMessage/ChatMessageStyled.js
@@ -3,10 +3,13 @@ import { Regular10Font } from "../../../fonts/Fonts";
 import { rem } from "../../../../utils/utils";
 
 const ChatMessageStyled = styled(Regular10Font)`
+  display: inline-block;
   min-width: ${rem("210px")};
+  width: 100%;
   margin-top: ${rem("-15px")};
   margin-bottom: 0;
   text-align: justify;
+  overflow-wrap: break-word;
 `;
 
 export default ChatMessageStyled;
